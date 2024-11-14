8ca6276445145b25005afcd6446bcdbf81519ad3
Ângelo Marques <angeloemanuelmarques@gmail.com>
2024-11-13 23:53:53 -0300

    feat: set up dark theme

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,10 +1,19 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useState } from "react";
+
+// const ClientThemeProvider = dynamic(() => import(''), { ssr: false });
 
 export function Providers({ children }: React.PropsWithChildren) {
   const [client] = useState(new QueryClient());
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+      </NextThemesProvider>
+    </QueryClientProvider>
+  );
 }
