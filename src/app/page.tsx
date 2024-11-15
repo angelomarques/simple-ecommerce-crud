77f1d2e656d0c34cf9137f9432b71f09ddca9b54
Ângelo Marques <angeloemanuelmarques@gmail.com>
2024-11-14 21:03:41 -0300

    feat: first version of header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Header />
+      Hello world
+    </div>
+  );
+}
+
+export function Header() {
+  return (
+    <header className="w-full border-b border-b-slate-500">
+      <div className="max-w-4xl mx-auto py-4">
+        <h1 className="text-3xl font-bold">Simple Store</h1>
+      </div>
+    </header>
+  );
 }
