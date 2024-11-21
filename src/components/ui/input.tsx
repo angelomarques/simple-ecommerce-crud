dc303b7fbf50ebf702a6e6c9ece091c262539a77
Ângelo Marques <angeloemanuelmarques@gmail.com>
2024-11-21 00:16:13 -0300

    feat: update header for search input and avatar

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,22 +1,33 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
+interface Props extends React.ComponentProps<"input"> {
+  suffix?: React.ReactNode;
+}
+
+const Input = React.forwardRef<HTMLInputElement, Props>(
+  ({ className, suffix, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = "Input"
+      <div className="relative">
+        <input
+          className={cn(
+            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
 
-export { Input }
+        {suffix && (
+          <span className="absolute right-2 top-1/2 h-full -translate-y-1/2 text-muted-foreground pointer-events-none flex items-center">
+            {suffix}
+          </span>
+        )}
+      </div>
+    );
+  }
+);
+Input.displayName = "Input";
+
+export { Input };
