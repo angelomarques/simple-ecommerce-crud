e15b87ab293d3eaafd593630f2951ca2efafef44
Ângelo Marques <angeloemanuelmarques@gmail.com>
2024-11-21 00:06:45 -0300

    refactor: avatar component to composition pattern

---
+++ src/components/ui/avatar/index.ts
@@ -0,0 +1,9 @@
+import { AvatarFallback } from "./fallback";
+import { AvatarImage } from "./image";
+import { AvatarRoot } from "./root";
+
+export const Avatar = {
+  Root: AvatarRoot,
+  Image: AvatarImage,
+  Fallback: AvatarFallback,
+};
