1ec4c9b2e0bc53024ef23b31a670d24a54ff0a42
Ângelo Marques <angeloemanuelmarques@gmail.com>
2024-11-23 23:39:07 -0300

    feat: dropdown menu for options

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,10 +2,11 @@
 
 import { useProductsStore } from "@/store/products";
 import { useUserStore } from "@/store/user";
-import { User } from "lucide-react";
+import { Plus, ShoppingCart, SlidersHorizontal, User } from "lucide-react";
 import { Avatar } from "./ui/avatar";
 import { Button } from "./ui/button";
 import { SearchInput } from "./ui/search-input";
+import { DropdownMenu } from "./ui/dropdown-menu";
 
 export function Header() {
   const setProductSearch = useProductsStore((state) => state.setQuerySearch);
@@ -38,6 +39,8 @@ export function Header() {
             {getViewButtonText()}
           </Button>
 
+          <HeaderMenu />
+
           <Avatar.Root>
             <Avatar.Image src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
             <Avatar.Fallback>
@@ -49,3 +52,36 @@ export function Header() {
     </header>
   );
 }
+
+const userMenuItems = [
+  { label: "My Cart", icon: <ShoppingCart /> },
+  { label: "Profile", icon: <User /> },
+];
+
+const adminMenuItems = [{ label: "New Product", icon: <Plus /> }];
+
+const HeaderMenu = () => {
+  const userView = useUserStore((state) => state.view);
+
+  const menuItems = userView === "admin" ? adminMenuItems : userMenuItems;
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <Button variant="outline">
+          <SlidersHorizontal />
+        </Button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content>
+        {menuItems.map(({ label, icon: Icon }) => (
+          <DropdownMenu.Item key={label}>
+            {Icon}
+
+            <span>{label}</span>
+          </DropdownMenu.Item>
+        ))}
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  );
+};
