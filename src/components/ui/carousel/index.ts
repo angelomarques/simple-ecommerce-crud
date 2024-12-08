c73b443ec84acfb092cb373708505a11639eabe9
Ângelo Marques <angeloemanuelmarques@gmail.com>
2024-12-08 13:49:18 -0300

    refactor: carousel component to composition pattern

---
+++ src/components/ui/carousel/index.ts
@@ -0,0 +1,11 @@
+import { CarouselContent } from "./content";
+import { CarouselItem } from "./item";
+import { CarouselPrevious } from "./previous";
+import { CarouselRoot } from "./root";
+
+export const Carousel = {
+  Root: CarouselRoot,
+  Content: CarouselContent,
+  Item: CarouselItem,
+  Previous: CarouselPrevious,
+};
---
+++ src/components/ui/carousel/types.ts
@@ -0,0 +1,15 @@
+import useEmblaCarousel, {
+  type UseEmblaCarouselType,
+} from "embla-carousel-react";
+
+export type CarouselApi = UseEmblaCarouselType[1];
+export type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
+export type CarouselOptions = UseCarouselParameters[0];
+export type CarouselPlugin = UseCarouselParameters[1];
+
+export type CarouselProps = {
+  opts?: CarouselOptions;
+  plugins?: CarouselPlugin;
+  orientation?: "horizontal" | "vertical";
+  setApi?: (api: CarouselApi) => void;
+};
