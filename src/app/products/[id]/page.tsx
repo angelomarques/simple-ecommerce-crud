3538d1680289f926c1934a22f1e78a995e8b048c
Ângelo Marques <angeloemanuelmarques@gmail.com>
2024-12-24 22:18:42 -0300

    feat: add price to product details page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Carousel } from "@/components/ui/carousel";
+import { formatPrice } from "@/lib/utils";
 import { getProductById } from "@/service/products";
 import { ProductType } from "@/service/products/types";
 import Image from "next/image";
@@ -26,10 +27,15 @@ interface ProductDetailsContentProps {
   product: ProductType;
 }
 
+// TODO: Let layout similar to amazon: image at the left, the info at the right
 function ProductDetailsContent({ product }: ProductDetailsContentProps) {
+  const previousPrice = (1 + product.discountPercentage / 100) * product.price;
+
   return (
     <main className="max-w-6xl mx-auto py-5 w-full flex-1">
       <h1 className="text-4xl font-bold">{product.title}</h1>
+
+      <p className="text-lg mt-4 text-slate-300">{product.description}</p>
 
       <Carousel.Root className="w-3/5 mx-auto mt-12">
         <Carousel.Content>
@@ -45,6 +51,14 @@ function ProductDetailsContent({ product }: ProductDetailsContentProps) {
         <Carousel.Previous />
         <Carousel.Next />
       </Carousel.Root>
+
+      <div className="flex gap-0.5 items-end mt-5">
+        <p className="text-2xl ">{formatPrice(product.price)}</p>
+
+        <p className="text-slate-400 ml-2">
+          <s>{formatPrice(previousPrice)}</s>
+        </p>
+      </div>
     </main>
   );
 }
