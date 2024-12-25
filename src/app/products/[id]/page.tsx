0a3031b3dcdca8c23a95df92581cfd9e9c373093
Ângelo Marques <angeloemanuelmarques@gmail.com>
2024-12-25 19:42:06 -0300

    feat: create star rating component

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,6 +2,7 @@ import { Carousel } from "@/components/ui/carousel";
 import { formatPrice } from "@/lib/utils";
 import { getProductById } from "@/service/products";
 import { ProductType } from "@/service/products/types";
+import { Star } from "lucide-react";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
@@ -33,32 +34,57 @@ function ProductDetailsContent({ product }: ProductDetailsContentProps) {
 
   return (
     <main className="max-w-6xl mx-auto py-5 w-full flex-1">
-      <h1 className="text-4xl font-bold">{product.title}</h1>
+      <div className="flex gap-16 mt-12">
+        <Carousel.Root className="w-3/5 mx-auto flex-[1.3]">
+          <Carousel.Content>
+            {product.images.map((image) => (
+              <Carousel.Item key={image}>
+                <div className="bg-slate-900 rounded-2xl">
+                  <Image src={image} alt="Product" width={600} height={600} />
+                </div>
+              </Carousel.Item>
+            ))}
+          </Carousel.Content>
 
-      <p className="text-lg mt-4 text-slate-300">{product.description}</p>
+          <Carousel.Previous />
+          <Carousel.Next />
+        </Carousel.Root>
 
-      <Carousel.Root className="w-3/5 mx-auto mt-12">
-        <Carousel.Content>
-          {product.images.map((image) => (
-            <Carousel.Item key={image}>
-              <div className="bg-slate-900 rounded-2xl">
-                <Image src={image} alt="Product" width={600} height={600} />
-              </div>
-            </Carousel.Item>
-          ))}
-        </Carousel.Content>
+        <div className="flex-1">
+          <h1 className="text-2xl font-bold">{product.title}</h1>
 
-        <Carousel.Previous />
-        <Carousel.Next />
-      </Carousel.Root>
+          <p className="mt-3 text-slate-300">{product.description}</p>
 
-      <div className="flex gap-0.5 items-end mt-5">
-        <p className="text-2xl ">{formatPrice(product.price)}</p>
+          <StarRating rating={product.rating} />
 
-        <p className="text-slate-400 ml-2">
-          <s>{formatPrice(previousPrice)}</s>
-        </p>
+          <div className="flex gap-0.5 items-end mt-5">
+            <p className="text-3xl ">{formatPrice(product.price)}</p>
+
+            <p className="text-slate-400 ml-2">
+              <s>{formatPrice(previousPrice)}</s>
+            </p>
+          </div>
+        </div>
       </div>
     </main>
+  );
+}
+
+interface StarRatingProps {
+  rating: number;
+}
+
+function StarRating({ rating }: StarRatingProps) {
+  return (
+    <div className="flex gap-1 mt-3">
+      {Array(5)
+        .fill(null)
+        .map((_, index) => (
+          <Star
+            key={index}
+            className="w-4 h-4 fill-yellow-500 text-yellow-500"
+          />
+        ))}
+    </div>
   );
 }
